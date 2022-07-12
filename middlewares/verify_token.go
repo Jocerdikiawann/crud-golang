6f17750586947189ca3650ce3166077f8ffcf73b
Nur Kholid <cocokcocok33@gmail.com>
2022-07-13 00:34:16 +0700

    fix: middleware

--- middlewares/verify_token.go
+++ middlewares/verify_token.go
@@ -3,7 +3,6 @@ package middlewares
 
 import (
 	"belajar-golang-rest-api/models/response"
-	"belajar-golang-rest-api/utils"
 	"fmt"
 	"net/http"
 	"os"
@@ -15,14 +14,17 @@ import (
 func MiddlewareAuth() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		header := c.GetHeader("Authorization")
-		token, err := jwt.Parse(header, func(t *jwt.Token) (interface{}, error) {
+
+		//parse token
+		token, errParseToken := jwt.Parse(header, func(t *jwt.Token) (interface{}, error) {
 			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
 				return nil, fmt.Errorf("unexpected signing method %v", t.Header["alg"])
 			}
 			return []byte(os.Getenv("SECRET_TOKEN")), nil
 		})
-		utils.IfErrorHandler(err)
-		if _, ok := token.Claims.(jwt.Claims); !ok && !token.Valid {
+
+		fmt.Println(token, errParseToken)
+		if errParseToken != nil {
 			res := response.WebResponse{
 				StatusCode: http.StatusUnauthorized,
 				Message:    "Unauthorized",
@@ -30,7 +32,20 @@ func MiddlewareAuth() gin.HandlerFunc {
 			}
 			c.JSON(res.StatusCode, res)
 			c.Abort()
+			return
 		}
+
+		//if _, ok := token.Claims.(jwt.Claims); !ok && !token.Valid {
+		//	fmt.Println("ok")
+		//	res := response.WebResponse{
+		//		StatusCode: http.StatusUnauthorized,
+		//		Message:    "Unauthorized",
+		//		Data:       gin.H{},
+		//	}
+		//	c.JSON(res.StatusCode, res)
+		//	c.Abort()
+		//	return
+		//}
 		c.Next()
 	}
 }
